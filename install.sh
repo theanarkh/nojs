8b0a007541eb66fbfd09d785fb341d98e0f3b96b
theanarkh <theratliter@gmail.com>
2024-10-09 01:15:02 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ set -e
 # # get and compile v8
 # gclient config https://github.com/v8/v8.git
 # gclient sync
+
+# compile v8 
 cd deps/v8 && tools/dev/v8gen.py x64.release.sample 
 ../depot_tools/ninja -C out.gn/x64.release.sample v8_monolith && cd ../..
 
